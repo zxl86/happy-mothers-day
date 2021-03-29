96631473c200781a277cf5b827ca2820c1c65d4e
zxl86 <cecilia.zhu04@gmail.com>
2021-03-29 20:59:13 +1100

    Refactored js code and added two vendor prefixes in css file.

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 // For carousels, arrow gets hidden when there are no more items to show in that scrolling direction
 const toggleArrows = (list, prevArrow, nextArrow) => {
   const scrolledLeft = list.scrollLeft;
-  console.log(scrolledLeft);
+
   if (scrolledLeft <= 5) {
     prevArrow.classList.add('hidden');
   }
@@ -20,115 +20,63 @@ const toggleArrows = (list, prevArrow, nextArrow) => {
 };
 
 // For cards carousel, the left and right cards in view gets transformed in order to simulate a 3D effect.
-const addScrollEffect = (allCarouselItems, arrowName) => {
-  const itemsArr = Array.from(allCarouselItems);
+const simulate3dEffect = (arrowName, carouselName) => {
+  const allItems = document.querySelectorAll(`.${carouselName} ul li`);
+  const itemsArr = Array.from(allItems);
   const leftIndex = itemsArr.findIndex((el) => el.classList.contains('left'));
   const rightIndex = itemsArr.findIndex((el) => el.classList.contains('right'));
 
   const changeSideCards = (shiftValue) => {
-    allCarouselItems[leftIndex]?.classList.remove('left');
-    allCarouselItems[leftIndex + shiftValue]?.classList.add('left');
-    allCarouselItems[rightIndex]?.classList.remove('right');
-    allCarouselItems[rightIndex + shiftValue]?.classList.add('right');
+    allItems[leftIndex]?.classList.remove('left');
+    allItems[leftIndex + shiftValue]?.classList.add('left');
+    allItems[rightIndex]?.classList.remove('right');
+    allItems[rightIndex + shiftValue]?.classList.add('right');
   };
 
-  if (arrowName === 'next') {
-    changeSideCards(1);
-  }
-
-  if (arrowName === 'prev') {
-    changeSideCards(-1);
-  }
+  arrowName === 'next' ? changeSideCards(1) : changeSideCards(-1);
 };
 
-// Recipes carousel
-const handleRecipesCarousel = () => {
+// General Carousel
+const handleCarousel = (carouselName, timeout, additionalEffects) => {
   const arrows = document.querySelectorAll(
-    '.recipes-carousel .arrow-container .arrow'
+    `.${carouselName} .arrow-container .arrow`
   );
   const prevArrow = document.querySelector(
-    '.recipes-carousel .arrow-container .arrow.prev'
+    `.${carouselName} .arrow-container .arrow.prev`
   );
   const nextArrow = document.querySelector(
-    '.recipes-carousel .arrow-container .arrow.next'
+    `.${carouselName} .arrow-container .arrow.next`
   );
-  const recipesList = document.querySelector('.recipes-carousel .recipes-list');
-  const recipe = document.querySelector(
-    '.recipes-carousel .recipes-list .recipe'
-  );
+  const itemsList = document.querySelector(`.${carouselName} ul`);
+  const item = document.querySelector(`.${carouselName} ul li`);
+  let timer = null;
 
   const arrowClickHandler = (e) => {
-    const classList = e.target.classList;
-    const scrollValue = classList.contains('next')
-      ? recipe.clientWidth
-      : -recipe.clientWidth;
-
-    recipesList.scrollBy({
+    const arrowName = e.target.classList.contains('prev') ? 'prev' : 'next';
+    // When arrow is clicked, scroll by one item width.
+    // Direction depends on which arrow is clicked.
+    const scrollValue =
+      arrowName === 'next' ? item.clientWidth : -item.clientWidth;
+    itemsList.scrollBy({
       top: 0,
       left: scrollValue,
       behavior: 'smooth',
     });
 
-    let timer = null;
+    // Additional effects are called here.
+    if (additionalEffects) {
+      additionalEffects.forEach((func) => func(arrowName, carouselName));
+    }
+
+    // Scrolling takes some time to complete, which would affect toggleArrows function.
+    // So setTimeout is used to delay toggleArrows to get the correct scrollLeft value.
     if (timer) {
       clearTimeout(timer);
     }
+
     timer = setTimeout(() => {
-      toggleArrows(recipesList, prevArrow, nextArrow);
-    }, 300);
-  };
-
-  arrows.forEach((arrowEl) =>
-    arrowEl.addEventListener('click', arrowClickHandler)
-  );
-};
-
-// Cards carousel
-const handleCardsCarousel = () => {
-  const arrows = document.querySelectorAll(
-    '.cards-carousel .arrow-container .arrow'
-  );
-  const prevArrow = document.querySelector(
-    '.cards-carousel .arrow-container .arrow.prev'
-  );
-  const nextArrow = document.querySelector(
-    '.cards-carousel .arrow-container .arrow.next'
-  );
-  const cardsList = document.querySelector('.cards-carousel .cards-list');
-  const card = document.querySelector('.cards-carousel .cards-list .card');
-
-  const arrowClickHandler = (e) => {
-    const allCards = document.querySelectorAll('.cards .cards-list .card');
-    const classList = e.target.classList;
-    const scrollValue = classList.contains('next')
-      ? card.clientWidth
-      : -card.clientWidth;
-
-    const getArrowName = () => {
-      if (classList.contains('prev')) {
-        return 'prev';
-      }
-
-      if (classList.contains('next')) {
-        return 'next';
-      }
-    };
-
-    cardsList.scrollBy({
-      top: 0,
-      left: scrollValue,
-      behavior: 'smooth',
-    });
-
-    addScrollEffect(allCards, getArrowName());
-
-    let timer = null;
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      toggleArrows(cardsList, prevArrow, nextArrow);
-    }, 500);
+      toggleArrows(itemsList, prevArrow, nextArrow);
+    }, timeout);
   };
 
   arrows.forEach((arrowEl) =>
@@ -175,8 +123,8 @@ const handleCardsDownload = () => {
 const _main = () => {
   'use strict';
 
-  handleRecipesCarousel();
-  handleCardsCarousel();
+  handleCarousel('recipes-carousel', 300);
+  handleCarousel('cards-carousel', 500, [simulate3dEffect]);
   handleCardsDownload();
 };
 
